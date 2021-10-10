7ec9a06d33f58da0b2d9b2bf774fd75d02580aaa
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-10-09 22:44:23 -0600

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -1,1 +1,6 @@
-
+# Deploy model as web service
+# (1) create deployment folder
+# (2) write entry/scoring script in the folder
+# (3) InferenceConfig()
+# (4) web service container: AciWebservice.deploy_configuration()
+# (5) deploy the model as a service
