9dbc3bbab28cbfa4f3aff7b3f0446f6b6778e16e
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-10-09 22:33:58 -0600

    Update Compute.py

--- Compute.py
+++ Compute.py
@@ -1,1 +1,17 @@
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core.compute_target import ComputeTargetException
 
+cluster_name = "your-compute-cluster"
+
+try:
+    # Check for existing compute target
+    training_cluster = ComputeTarget(workspace=ws, name=cluster_name)
+    print('Found existing cluster, use it.')
+except ComputeTargetException:
+    # If it doesn't already exist, create it
+    try:
+        compute_config = AmlCompute.provisioning_configuration(vm_size='STANDARD_DS11_V2', max_nodes=2)
+        training_cluster = ComputeTarget.create(ws, cluster_name, compute_config)
+        training_cluster.wait_for_completion(show_output=True)
+    except Exception as ex:
+        print(ex)
