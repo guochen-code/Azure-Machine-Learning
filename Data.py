9230fe7870d42ae9095245a1d2bb07e2c900f9ca
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-10-09 22:33:21 -0600

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,1 +1,25 @@
+from azureml.core import Dataset
 
+default_ds = ws.get_default_datastore()
+
+if 'diabetes dataset' not in ws.datasets:
+    default_ds.upload_files(files=['./data/diabetes.csv', './data/diabetes2.csv'], # Upload the diabetes csv files in /data
+                        target_path='diabetes-data/', # Put it in a folder path in the datastore
+                        overwrite=True, # Replace existing files of the same name
+                        show_progress=True)
+
+    #Create a tabular dataset from the path on the datastore (this may take a short while)
+    tab_data_set = Dataset.Tabular.from_delimited_files(path=(default_ds, 'diabetes-data/*.csv'))
+
+    # Register the tabular dataset
+    try:
+        tab_data_set = tab_data_set.register(workspace=ws, 
+                                name='diabetes dataset',
+                                description='diabetes data',
+                                tags = {'format':'CSV'},
+                                create_new_version=True)
+        print('Dataset registered.')
+    except Exception as ex:
+        print(ex)
+else:
+    print('Dataset already registered.')
