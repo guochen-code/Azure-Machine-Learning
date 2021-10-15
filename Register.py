95a0f40d72ed1cc338138655a63eb2d16bca3212
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-10-15 14:09:00 -0600

    Update Register.py

--- Register.py
+++ Register.py
@@ -5,6 +5,7 @@ urllib.request.urlretrive('https://....','model.onnx')
 model=Model.register(ws,model_name='model_name',model_path='./model_name') ### model_path refers to local path
 
 #################################### register a model from run
+#                         (1) Register a model from an azureml.core.Run object:
 # save model, automatically uploaded
 os.makedirs('outputs',exist_ok=True)
 joblib.dump(value=model,filename='outputs/model_name.pkl')
@@ -13,6 +14,18 @@ run.register_model(model_path='outputs/model_name.pkl',  ### model_path refers t
                    model_name='model_name',
                    tags={'Training Contect':'Tabular dataset'},
                    properties={'AUC':run.get_metrics()['AUC'],'Accuracy':run.get_metrics()['Accuracy']})
+
+print(model.name, model.id, model.version, sep='\t')
+
+#                        (2) Register a model from an azureml.train.automl.run.AutoMLRun object:
+description = 'My AutoML Model'
+model = run.register_model(description = description,
+                               tags={'area': 'qna'})
+
+print(run.model_id)
+# In this example, the metric and iteration parameters aren't specified, so the iteration with the best primary metric will be registered. 
+# The model_id value returned from the run is used instead of a model name.
+
 ###################################### display registered model
 from azureml.core import Model
 for model in Model.list(ws):
