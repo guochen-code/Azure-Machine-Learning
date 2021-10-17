23ef0b4d4e3e60840a8aaaf33122bac9ddb86f56
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-10-17 09:05:40 -0600

    Create Authentication_2.py

---
+++ Authentication_2.py
@@ -0,0 +1,11 @@
+'''Interactive Login Authentication
+Azure CLI Authentication
+Managed Service Identity (MSI) Authentication
+Service Principal Authentication
+Token Authentication
+The interactive authentication is suitable for local experimentation on your own computer. 
+Azure CLI authentication is suitable if you are already using Azure CLI for managing Azure resources, and want to sign in only once. 
+The MSI and Service Principal authentication are suitable for automated workflows, for example as part of Azure Devops build.
+'''
+
+
