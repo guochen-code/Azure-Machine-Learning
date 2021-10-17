50ebd36dcb185315cff454ca3e68b6af1c401b22
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-10-16 21:29:28 -0600

    Update Environment.py

--- Environment.py
+++ Environment.py
@@ -15,3 +15,35 @@ service_env=Environment(name='service_env')
 python_packages=['scikit-learn','azureml-defaults','azure-ml-api-sdk']
 for package in packages:
   service_env.python.conda_dependencies.add_pip_package(package)
+                         
+                               
+*******************************************************************
+# Use a RunConfiguration to specify some additional requirements for this step.
+from azureml.core.runconfig import RunConfiguration
+from azureml.core.conda_dependencies import CondaDependencies
+from azureml.core.runconfig import DEFAULT_CPU_IMAGE
+
+# create a new runconfig object
+run_config = RunConfiguration()
+
+# enable Docker 
+run_config.environment.docker.enabled = True
+
+# set Docker base image to the default CPU-based image
+run_config.environment.docker.base_image = DEFAULT_CPU_IMAGE
+
+# use conda_dependencies.yml to create a conda environment in the Docker image for execution
+run_config.environment.python.user_managed_dependencies = False
+
+# specify CondaDependencies obj
+run_config.environment.python.conda_dependencies = CondaDependencies.create(conda_packages=['scikit-learn'])
+
+# For this step, we use yet another source_directory
+source_directory = './extract'
+print('Source directory for the step is {}.'.format(os.path.realpath(source_directory)))
+
+step = PythonScriptStep(name="extract_step",
+                         script_name="extract.py", 
+                         compute_target=aml_compute, 
+                         source_directory=source_directory,
+                         runconfig=run_config)
