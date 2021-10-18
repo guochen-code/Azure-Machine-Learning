fd733ce8443f3d2ecf7e588867651c7765be00ed
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-10-18 07:31:24 -0600

    Create AutoML.py

---
+++ AutoML.py
@@ -0,0 +1,9 @@
+'''
+ONLY connect to local data files or azure blob storage.
+ONLY accepts azure ml TabularDatasets when working on a remote compute.
+Train,validation, and test:
+>20,000: 10%
+<20,000:
+  <1,000: 10 folds
+  1,000-20,000: 3 folds
+'''
