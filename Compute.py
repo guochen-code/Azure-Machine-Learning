eb7508a850094c5c22e840f301d9f7b973a66d84
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-10-29 11:29:32 -0600

    Update Compute.py

--- Compute.py
+++ Compute.py
@@ -1,3 +1,8 @@
+pass compute_target into ScriptRunStep:
+(1) using name: compute_target = compute_name
+(2) using ComputeTarget Object: compute_target = training_cluster
+
+********************************************************************************* Part I *********************************************************************************
 from azureml.core.compute import ComputeTarget, AmlCompute
 from azureml.core.compute_target import ComputeTargetException
 
@@ -20,3 +25,17 @@ except ComputeTargetException:
   *********************************************************************************
 # For a more detailed view of current Azure Machine Learning Compute status, use get_status()
 training_cluster.get_status().serialize()
+
+********************************************************************************* Part II *********************************************************************************
+from azureml.core import Workspace
+from azureml.core.compute import ComputeTarget, DatabricksCompute
+ws=Workspace.from_config()
+compute_name='db_cluster
+db_workspace_name='db_workspace'
+db_resource_group='db_resource_group'
+db_access_token='1234-abc-5678-defg-90...'
+db_config=DatabricksCompute.attach_configuration(resource_group=db_resource_group,
+                                                 workspace_name=db_workspace_name,
+                                                 access_token=db_access_token)
+databricks_compute=ComputeTarget.attach(ws,compute_name,db_config)
+databricks_compute.wait_for_completion(True)
