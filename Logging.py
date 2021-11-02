3b5c136a5e643fe6b4a1b4b468a6b6ce1850e98c
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-02 09:44:26 -0600

    Create Logging.py

---
+++ Logging.py
@@ -0,0 +1,28 @@
+# Count the rows and log the result
+row_count = (len(data))
+run.log('observations', row_count)
+print('Analyzing {} rows of data'.format(row_count))
+
+# Plot and log the count of diabetic vs non-diabetic patients
+diabetic_counts = data['Diabetic'].value_counts()
+fig = plt.figure(figsize=(6,6))
+ax = fig.gca()    
+diabetic_counts.plot.bar(ax = ax) 
+ax.set_title('Patients with Diabetes') 
+ax.set_xlabel('Diagnosis') 
+ax.set_ylabel('Patients')
+plt.show()
+run.log_image(name='label distribution', plot=fig)
+
+# log distinct pregnancy counts
+pregnancies = data.Pregnancies.unique()
+run.log_list('pregnancy categories', pregnancies)
+
+# Log summary statistics for numeric columns
+med_columns = ['PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI']
+summary_stats = data[med_columns].describe().to_dict()
+for col in summary_stats:
+    keys = list(summary_stats[col].keys())
+    values = list(summary_stats[col].values())
+    for index in range(len(keys)):
+        run.log_row(col, stat=keys[index], value = values[index])
