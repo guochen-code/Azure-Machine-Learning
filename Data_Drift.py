45ac34fc69e574a9d2e102753fe78256db0b10a0
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-01 20:56:23 -0600

    Create Data_Drift.py

---
+++ Data_Drift.py
@@ -0,0 +1,43 @@
+# (1) baseline dataset
+# (2) create target dataset
+from azureml.core import Workspace, Dataset
+from azureml.datadrift import DataDriftDetector
+from datetime import datetime
+
+# get the workspace object
+ws = Workspace.from_config()
+
+# get the target dataset
+target = Dataset.get_by_name(ws, 'target')
+
+# set the baseline dataset
+baseline = target.time_before(datetime(2019, 2, 1))
+
+# set up feature list
+features = ['latitude', 'longitude', 'elevation', 'windAngle', 'windSpeed', 'temperature', 'snowDepth', 'stationName', 'countryOrRegion']
+
+# set up data drift detector
+monitor = DataDriftDetector.create_from_datasets(ws, 'drift-monitor', baseline, target, 
+                                                      compute_target='cpu-cluster', 
+                                                      frequency='Week', 
+                                                      feature_list=None, 
+                                                      drift_threshold=.6, 
+                                                      latency=24)
+
+# get data drift detector by name
+monitor = DataDriftDetector.get_by_name(ws, 'drift-monitor')
+
+# update data drift detector
+monitor = monitor.update(feature_list=features)
+
+# run a backfill for January through May
+backfill1 = monitor.backfill(datetime(2019, 1, 1), datetime(2019, 5, 1))
+
+# run a backfill for May through today
+backfill1 = monitor.backfill(datetime(2019, 5, 1), datetime.today())
+
+# disable the pipeline schedule for the data drift detector
+monitor = monitor.disable_schedule()
+
+# enable the pipeline schedule for the data drift detector
+monitor = monitor.enable_schedule()
