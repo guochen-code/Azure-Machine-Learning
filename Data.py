e5006c05747e21a6a63b5278884b5e60c01c4292
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-02 12:53:00 -0600

    Update Data.py

--- Data.py
+++ Data.py
@@ -46,3 +46,59 @@ estimator=Estimator(...,
                     inputs=[img_ds.as_named_input('img_data').as_download(path_on_compute='data')],
                     ....)
             
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Train a model from a file dataset
+# When you're using a file dataset, the dataset argument passed to the script represents a mount point containing file paths. 
+# How you read the data from these files depends on the kind of data in the files and what you want to do with it. 
+# In the case of the diabetes CSV files, you can use the Python glob module to create a list of files in the virtual mount point defined by the dataset, 
+# and read them all into Pandas dataframes that are concatenated into a single dataframe.
+
+# Get script arguments (rgularization rate and file dataset mount point)
+parser = argparse.ArgumentParser()
+parser.add_argument('--regularization', type=float, dest='reg_rate', default=0.01, help='regularization rate')
+parser.add_argument('--input-data', type=str, dest='dataset_folder', help='data mount point')
+args = parser.parse_args()
+
+# Set regularization hyperparameter (passed as an argument to the script)
+reg = args.reg_rate
+
+data_path = run.input_datasets['training_files'] # Get the training data path from the input
+# (You could also just use args.dataset_folder if you don't want to rely on a hard-coded friendly name)
+
+# Read the files
+all_files = glob.glob(data_path + "/*.csv")
+diabetes = pd.concat((pd.read_csv(f) for f in all_files), sort=False)
+
+# Just as with tabular datasets, you can retrieve a file dataset from the input_datasets collection by using its friendly name. 
+# You can also retrieve it from the script argument, which in the case of a file dataset contains a mount path to the files 
+# (rather than the dataset ID passed for a tabular dataset).
+
+# Next we need to change the way we pass the dataset to the script - it needs to define a path from which the script can read the files. 
+# You can use either the as_download or as_mount method to do this. 
+# Using as_download causes the files in the file dataset to be downloaded to a temporary location on the compute where the script is being run, 
+# while as_mount creates a mount point from which the files can be streamed directly from the datastore.
+
+# You can combine the access method with the as_named_input method to include the dataset in the input_datasets collection in the experiment run 
+# (if you omit this, for example by setting the argument to diabetes_ds.as_mount(), 
+# the script will be able to access the dataset mount point from the script arguments, but not from the input_datasets collection).
+
+from azureml.core import Experiment
+from azureml.widgets import RunDetails
+
+
+# Get the training dataset
+diabetes_ds = ws.datasets.get("diabetes file dataset")
+
+# Create a script config
+script_config = ScriptRunConfig(source_directory=experiment_folder,
+                                script='diabetes_training.py',
+                                arguments = ['--regularization', 0.1, # Regularizaton rate parameter
+                                             '--input-data', diabetes_ds.as_named_input('training_files').as_download()], # Reference to dataset location
+                                environment=env) # Use the environment created previously
+
+# submit the experiment
+experiment_name = 'mslearn-train-diabetes'
+experiment = Experiment(workspace=ws, name=experiment_name)
+run = experiment.submit(config=script_config)
+RunDetails(run).show()
+run.wait_for_completion()
