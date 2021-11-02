d51efb86fe0fddad546920d53c26187afb703bb7
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-01 21:02:44 -0600

    Update Data_Drift.py

--- Data_Drift.py
+++ Data_Drift.py
@@ -41,3 +41,22 @@ monitor = monitor.disable_schedule()
 
 # enable the pipeline schedule for the data drift detector
 monitor = monitor.enable_schedule()
+***************************understanding data drift results
+# Metrics:
+(1) Data drift magnitude
+(2) Top drifting features: Shows the features from the dataset that have drifted the most and are therefore contributing the most to the Drift Magnitude metric.
+  2.1 - Numeric features: Wasserstein distance/mean/min/max
+  2.2 - Categorical features: Euclidian distance/Unique values
+(3) Threshold: Data Drift magnitude beyond the set threshold will trigger alerts. This can be configured in the monitor settings.
+
+***************************Trouleshooting
+# If the SDK backfill() function does not generate the expected output, it may be due to an authentication issue. 
+# When you create the compute to pass into this function, do not use Run.get_context().experiment.workspace.compute_targets. 
+# Instead, use ServicePrincipalAuthentication such as the following to create the compute that you pass into that backfill() function:
+auth = ServicePrincipalAuthentication(
+        tenant_id=tenant_id,
+        service_principal_id=app_id,
+        service_principal_password=client_secret
+        )
+ws = Workspace.get("xxx", auth=auth, subscription_id="xxx", resource_group="xxx")
+compute = ws.compute_targets.get("xxx")
