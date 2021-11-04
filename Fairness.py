9c3937c12c030f297c147776f29e4a92026bbd27
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-04 17:04:05 -0600

    Create Fairness.py

---
+++ Fairness.py
@@ -0,0 +1,12 @@
+The Fairlearn open-source package provides postprocessing and reduction unfairness mitigation algorithms:
+  
+(1) ExponentiatedGradient - reduction - Binary classification and regression - Demographic parity, equalized odds
+(2) GridSearch - reduction -Binary classification and regression - Demographic parity, equalized odds / Bounded group loss
+(3) ThresholdOptimizer - postprocessing - Binary classification - Demographic parity, equalized odds
+
+Demographic parity: any
+True positive rate parity: any
+False-positive rate parity: any
+Equalized odds: any
+Error rate parity: reduction-based mitigation algorithms (Exponentiated Gradient and Grid Search) 
+Bounded group loss: reduction-based mitigation algorithms (Exponentiated Gradient and Grid Search) 
