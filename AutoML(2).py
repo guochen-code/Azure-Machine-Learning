18ee907e090c7ea2d3d5f48c9545487a82f0cbe2
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-04 11:07:34 -0600

    Update AutoML(2).py

--- AutoML(2).py
+++ AutoML(2).py
@@ -16,6 +16,23 @@ for metric_name in best_run_metrics:
 # Exploring preprocessing steps. View those steps in the fitted_model obtaind from the best run:
 for step in fitted_model.named_steps:
   print(step)
+********************************************************************************************************************************* For Deployment Use
+# Download the conda environment file
+from azureml.automl.core.shared import constants
+conda_env_file_name = 'conda_env.yml'
+best_run.download_file(name="outputs/conda_env_v_1_0_0.yml", output_file_path=conda_env_file_name)
+with open(conda_env_file_name, "r") as conda_file:
+    conda_file_contents = conda_file.read()
+    print(conda_file_contents)
+    
+# Download the model scoring file   
+from azureml.automl.core.shared import constants
+script_file_name = 'scoring_file.py'
+best_run.download_file(name="outputs/scoring_file_v_1_0_0.py", output_file_path=script_file_name)
+with open(script_file_name, "r") as scoring_file:
+    scoring_file_contents = scoring_file.read()
+    print(scoring_file_contents)  
 
-# 
-  
+# Deployment
+myenv = Environment.from_conda_specification(name="myenv", file_path=conda_env_file_name)
+inference_config = InferenceConfig(entry_script=script_file_name, environment=myenv)
