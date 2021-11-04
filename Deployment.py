1a3908c8141cc3c50dda0905b4f50ff985ce191a
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-04 11:55:09 -0600

    Update Deployment.py

--- Deployment.py
+++ Deployment.py
@@ -39,3 +39,72 @@ service.wait_for_completion(show_output=True)
                      
 *************CLI
 az ml model deploy --ct myaks -m mymodel:1 -n myservice --ic inferenceconfig.json --dc deploymentconfig.json
+
+********************************************************************************************
+# Reload Service
+
+# You can update your score.py file and then call reload() to quickly restart the service. 
+# This will only reload your execution script and dependency files, it will not rebuild the underlying Docker image. 
+# As a result, reload() is fast, but if you do need to rebuild the image -- to add a new Conda or pip package, for instance -- you will have to call update()
+local_service.reload()
+
+# If you want to change your model(s), Conda dependencies or deployment configuration, call update() to rebuild the Docker image.
+# Update Service
+local_service.update(models=[model],
+                     inference_config=inference_config,
+                     deployment_config=deployment_config)
+
+
+******************************** Deploy model to AKS cluster based on the LocalWebservice's configuration ****************************************************************
+# This is a one time setup for AKS Cluster. You can reuse this cluster for multiple deployments after it has been created. 
+# If you delete the cluster or the resource group that contains it, then you would have to recreate it.
+from azureml.core.compute import AksCompute, ComputeTarget
+from azureml.core.compute_target import ComputeTargetException
+
+# Choose a name for your AKS cluster
+aks_name = 'my-aks-9' 
+
+# Verify the cluster does not exist already
+try:
+    aks_target = ComputeTarget(workspace=ws, name=aks_name)
+    print('Found existing cluster, use it.')
+except ComputeTargetException:
+    # Use the default configuration (can also provide parameters to customize)
+    prov_config = AksCompute.provisioning_configuration()
+
+    # Create the cluster
+    aks_target = ComputeTarget.create(workspace = ws, 
+                                      name = aks_name, 
+                                      provisioning_configuration = prov_config)
+
+if aks_target.get_status() != "Succeeded":
+    aks_target.wait_for_completion(show_output=True)
+
+
+from azureml.core.webservice import AksWebservice
+# Set the web service configuration (using default here)
+aks_config = AksWebservice.deploy_configuration()
+
+# # Enable token auth and disable (key) auth on the webservice
+# aks_config = AksWebservice.deploy_configuration(token_auth_enabled=True, auth_enabled=False)
+
+%%time
+aks_service_name ='aks-service-1'
+
+aks_service = local_service.deploy_to_cloud(name=aks_service_name,
+                                            deployment_config=aks_config,
+                                            deployment_target=aks_target)
+
+aks_service.wait_for_deployment(show_output = True)
+print(aks_service.state)
+
+# Test aks service
+
+sample_input = json.dumps({
+    'data': dataset_x[0:2].tolist()
+})
+
+aks_service.run(sample_input)
+
+# Delete the service if not needed.
+aks_service.delete()
