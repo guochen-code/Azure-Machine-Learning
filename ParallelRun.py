6612ce42c500f2c54ebff1f3d570247fc3365dd3
guochen-code <83231868+guochen-code@users.noreply.github.com>
2021-11-04 21:50:20 -0600

    Update ParallelRun.py

--- ParallelRun.py
+++ ParallelRun.py
@@ -1,4 +1,5 @@
-# the class ParallelRunStep is used to defined and set up a pipeline step and build a batch inference pipeline to handle large amounts of data in parallel (break up data into batches)
+# the class ParallelRunStep is used to defined and set up a pipeline step and build a batch inference pipeline 
+# to handle large amounts of data in parallel (break up data into batches)
 
 from azureml.pipeline.steps import ParallelRunConfig, ParallelRunStep
 from azureml.data import OutputFileDatasetConfig
